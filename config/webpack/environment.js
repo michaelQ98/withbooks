0adc9ae9ad8e24bf449942bd98ba60be7df73f1f
michaelQ98 <poplarfactory@gmail.com>
2020-10-12 09:19:48 +0900

    初期設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+// Bootstrap設定
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+// ここまで
+
 module.exports = environment
